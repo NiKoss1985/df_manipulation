82ac3e78c36abafb26dfd66b9a32af5031fc0929
Nicola Rubino <rubino.nico@gmail.com>
2023-11-05 18:57:32 +0100

    Update project_manipulation.py

--- project_manipulation.py
+++ project_manipulation.py
@@ -1,1 +1,16 @@
+#import libraries 
+import pandas as pd
+import numpy as np 
+
+#upload .csv file in pandas 
+dataframe = pd.read_csv("sales.csv")
+
+#visualise top 5 rows to get an understanding of the DF and names of columns
+dataframe.head()
+
+#visualise dtypes to get an understanding of the DF
+dataframe.dtypes
+
+#use .info to find potential NaN values in the DF
+sales.info()
 
