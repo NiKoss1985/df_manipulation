8467eb79b94f58ba5b11fc61075b639e5a8ef858
Nicola Rubino <rubino.nico@gmail.com>
2023-11-05 19:10:39 +0100

    Update project_manipulation.py

--- project_manipulation.py
+++ project_manipulation.py
@@ -1,16 +1,20 @@
-#import libraries 
+# Import libraries 
 import pandas as pd
 import numpy as np 
 
-#upload .csv file in pandas 
-dataframe = pd.read_csv(r'github.com/NiKoss1985/df_manipulation/blob/main/db/sales.csv')
+# GitHub raw URL for the CSV file
+csv_raw_url = 'https://raw.githubusercontent.com/NiKoss1985/df_manipulation/main/db/sales.csv'
 
-#visualise top 5 rows to get an understanding of the DF and names of columns
+# Upload .csv file in pandas 
+dataframe = pd.read_csv(csv_raw_url)
+
+# Visualise top 5 rows to get an understanding of the DF and names of columns
 dataframe.head()
 
-#visualise dtypes to get an understanding of the DF
+# Visualise dtypes to get an understanding of the DF
 dataframe.dtypes
 
-#use .info to find potential NaN values in the DF
+# Use .info to find potential NaN values in the DF
 dataframe.info()
+
 
